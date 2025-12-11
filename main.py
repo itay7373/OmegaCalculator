f572482285b149d54d6c247707e729896392ddc0
Itay Efraim <itayefraim7373@example.com>
2025-12-11 23:15:33 +0200

    first dev commit

---
+++ main.py
@@ -0,0 +1,6 @@
+expression = input("Enter your expression (enter EXIT to exit): ")
+
+while expression != "EXIT":
+    expression = input("Enter your expression (enter EXIT to exit): ")
+
+print("thank you for using my calculator!")
