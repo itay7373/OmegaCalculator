593a51dd7a8f507fc4e59b147c20ab7685735faa
Itay Efraim <itayefraim7373@example.com>
2025-12-23 00:10:03 +0200

    first calculations code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from parser import find_number
+from parser import calc
 
 expression = input("Enter your expression (enter EXIT to exit): ")
 
-while expression != "EXIT":
-    print(find_number(expression))
-    expression = input("Enter your expression (enter EXIT to exit): ")
+while expression.upper() != "EXIT":
+  print(f"\nthe answer is: {calc(expression.replace(" ", ""))}\n---------------------------")
+  expression = input("Enter your expression (enter EXIT to exit): ")
 
 print("thank you for using my calculator!")
--- parser.py
+++ parser.py
@@ -1,14 +1,17 @@
+from operations import power_map, calculate_operation
+
+
 def find_number(expression, index=0):
     minus_counter = 0
     start_index = index
 
     #בודק אם יש ~ לפני המספר ומעדכן את מונה המינוסים בהתאם
-    if (expression[index] == '~'):
+    if expression[index] == '~':
         start_index += 1
         minus_counter = 1
 
     # סופר כמה מינוסים יש לפני המספר
-    while (expression[start_index] == '-'):
+    while expression[start_index] == '-':
         minus_counter += 1
         start_index += 1
 
@@ -29,3 +32,56 @@ def find_number(expression, index=0):
 
     #מחזיר את המספר, מטפל בשליליות וחיוביות ומחזיר את האינדקס הבא בביטוי המתמטי
     return (number, end_index) if minus_counter % 2 == 0 else (number * (-1), end_index)
+
+
+def insert_to_mat(mat, num, operand = None):
+  if operand in power_map:
+    mat[power_map[operand]].append(num)
+    mat[power_map[operand]].append(operand)
+
+
+def check_mat(mat, operand):
+  for i in range(len(mat) - 1, power_map[operand] - 1,-1):
+    if len(mat[i]) > 0:
+      return i
+  return -1
+
+
+def calc_mat(mat, num, operand = None):
+  index = check_mat(mat, operand)
+  if index != -1:
+    num2 = mat[index].pop(0)
+    operand2 = mat[index].pop()
+    new_reasult = calculate_operation(operand2, num2, num)
+    calc_mat(mat, new_reasult, operand)
+  else:
+    insert_to_mat(mat, num, operand)
+
+
+def calc(expression):
+  if len(expression) == 0:
+    print("the expression must not be empty")
+    return None
+
+  expression += '+0'
+
+  mat = [
+      [],
+      [],
+      [],
+      [],
+      []
+  ]
+
+  index = 0
+  num, index = find_number(expression, index)
+
+  while index < len(expression):
+    op = expression[index]
+    calc_mat(mat, num, op)
+    check_mat(mat,op)
+    index += 1
+    num, index = find_number(expression, index)
+
+
+  return mat[0][0]
